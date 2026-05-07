4e725d4f94cdc9c9c8904fb555985fcd523a1b47
Emilio-115 <emivazcru@alum.us.es>
2026-05-07 23:30:44 +0200

    feat: :sparkles: add optuna to tuner

--- src/utils/tuner.py
+++ src/utils/tuner.py
@@ -1,8 +1,9 @@
 import keras_tuner as kt
 from typing import Callable
 import os
+import optuna
 
-def get_tuner(model_builder: Callable, fold_idx, prolapse_name, experiment):
+def get_keras_tuner(model_builder: Callable, fold_idx, prolapse_name, experiment):
 
     BASE_DIR = os.path.abspath(
         os.path.join(os.path.dirname(__file__), "..", "..")
@@ -20,3 +21,33 @@ def get_tuner(model_builder: Callable, fold_idx, prolapse_name, experiment):
         project_name=project_name,
         overwrite=False
     )
+
+def get_optuna_study(fold_idx, prolapse_name, experiment,
+                     direction="maximize", persist=True, reset=False):
+    study_name = f"study_fold_{fold_idx}"
+
+    if persist:
+        BASE_DIR = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "..")
+        )
+        study_dir = os.path.join(BASE_DIR, "results", experiment, prolapse_name, "optuna_results")
+        os.makedirs(study_dir, exist_ok=True)
+        storage = f"sqlite:///{os.path.join(study_dir, 'study.db')}"
+
+        if reset:
+            try:
+                optuna.delete_study(study_name=study_name, storage=storage)
+                print(f"Estudio '{study_name}' eliminado.")
+            except KeyError:
+                pass
+    else:
+        storage = None
+
+    study = optuna.create_study(
+        study_name=study_name,
+        direction=direction,
+        storage=storage,
+        load_if_exists=persist and not reset,
+        sampler=optuna.samplers.TPESampler(seed=42),
+    )
+    return study
